7b140bcd93209c7743ad911316e6a95415e4c2ba
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-09-30 17:05:20 +0100

    Monday

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  'Hello!'
+end
+
+get '/secret' do
+  'I love chocolate seeds'
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+ </div>"
+end
